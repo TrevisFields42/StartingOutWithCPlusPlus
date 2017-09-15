78cf6cfe30a45cedc153a9154d74088b7f053e07
Trevis Jontaé Fields <trevis42@icloud.com>
2017-09-15 05:26:43 -0700

    Hello World
    Hello World Test Code

--- Chapter2/Chapter2/main.cpp
+++ Chapter2/Chapter2/main.cpp
@@ -1,15 +1,13 @@
-//
-//  main.cpp
-//  Chapter2
-//
-//  Created by Trevis Jontaé Fields on 9/15/17.
-//  Copyright © 2017 Trevis Jontaé Fields. All rights reserved.
-//
+  // Code Author: Trevis J Fields.
+  //Book: Starting Out With C++ Early Objects 7th Edition
+  // Date: Friday 15th September 2017
+  // Chapter 2 Introduction to C++
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-	return 0;
+int main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
 }
