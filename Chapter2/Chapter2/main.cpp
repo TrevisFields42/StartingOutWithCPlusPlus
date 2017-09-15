e843b02363dac49ecc4cd71966da4e4f4f2d8817
Trevis Jontaé Fields <trevis42@icloud.com>
2017-09-15 02:34:01 -0700

    Chapter 1 Notes
    Chapter 1.
    Section 1.1
    Section 1.2
    Checkpoint

---
+++ Chapter2/Chapter2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter2
+//
+//  Created by Trevis Jontaé Fields on 9/15/17.
+//  Copyright © 2017 Trevis Jontaé Fields. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
